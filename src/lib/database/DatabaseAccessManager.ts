a567fdcdbfe7b42b4ab1bbe5bd93c95736241060
Fabian Holler <fabianholler2005@gmail.com>
2023-05-24 16:12:41 +0200

    added types for the database insertion

--- src/lib/database/DatabaseAccessManager.ts
+++ src/lib/database/DatabaseAccessManager.ts
@@ -6,31 +6,52 @@ import { ref, set, push } from "firebase/database";
 import { database } from "./DatabaseConfig";
 
 /*
+ * Type definitions
+ */
+type Project = {
+  name: string,
+  description: string,
+}
+
+type User = {
+  firstname: string,
+  lastname: string,
+  email: string,
+}
+
+/*
  * The only function that directly writes into the database.
  * Every write action that needs to be performed needs to call this function
  */
-function writeIntoDatabase(path: string, values: Map<string, string>) {
+function writeIntoDatabase(path: string, values: Project | User) {
   const dbRef = ref(database, path);
   const newRef = push(dbRef);
   
   set(newRef, values);
-
 }
 
-export function createNewUser(firstname: string, lastname: string, email: string) {
-  const values = new Map();
-  
-  values.set("firstname", firstname)
-  values.set("lastname", lastname)
-  values.set("email", email)
-
-  writeIntoDatabase("Users/", values)
+/*
+ * Creates a new user. 
+ */
+export function createNewUser(firstname: string, lastname: string, email: string): void {
+  const values: User = {
+    firstname,
+    lastname,
+    email,
+  }
+   writeIntoDatabase("Users/", values)
 }
 
-export function storeProject(name: string, description: string) {
-  const values = {
-    projectname: name,
-    projectdescription: description
+/*
+ * Stores a new project in the database.
+ * TODO: add functionality for the project (path).
+ * TODO: automatically make the Project a child of the currently logged in user.
+ */
+
+export function storeProject(name: string, description: string): void {
+  const values: Project = {
+    name,
+   description
   }
   writeIntoDatabase("Users/Projects/", values)
 }
