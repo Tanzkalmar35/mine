6612bd2d94ad2c0ffcce3064b987796718c00246
Tanzkalmar35 <94364725+Tanzkalmar35@users.noreply.github.com>
2023-05-27 02:35:29 +0200

    preparing the login page -> next todo

--- src/App.svelte
+++ src/App.svelte
@@ -2,21 +2,28 @@
     import Dashboard from "./lib/AppComponents/Dashboard.svelte";
     import Settings from "./lib/AppComponents/Settings.svelte";
     import HomePage from "./lib/AppComponents/HomePage.svelte";
+    import LoginPage from "./lib/AppComponents/LoginPage.svelte";
 
     const currentUrl = window.location.pathname;
+    let loggedIn = false;
 
 </script>
 
 <main class="w-screen h-screen flex justify-center text-black bg-accent">
     <div class="w-full h-full absolute z-1" id="content">
-        {#if currentUrl === "/"}
-            <HomePage/>
+        {#if loggedIn}
+            {#if currentUrl === "/"}
+                <HomePage/>
+            {/if}
+            {#if currentUrl === "/Dashboard"}
+                <Dashboard/>
+            {/if}
+            {#if currentUrl === "/Settings"}
+                <Settings/>
+            {/if}
         {/if}
-        {#if currentUrl === "/Dashboard"}
-            <Dashboard/>
-        {/if}
-        {#if currentUrl === "/Settings"}
-            <Settings/>
+        {#if !loggedIn}
+            <LoginPage/>
         {/if}
     </div>
 </main>
