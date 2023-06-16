6af0991e3c1b6630c55c11c8dc9cdd741dbdc995
Tanzkalmar35 <94364725+Tanzkalmar35@users.noreply.github.com>
2023-06-16 21:39:15 +0200

    small restructure

--- src/lib/AppComponents/Login/LoginPage.svelte
+++ src/lib/AppComponents/Login/LoginPage.svelte
@@ -2,9 +2,6 @@
     import LoginHeroImage from "../../UiElements/Img/LoginHeroImage.svelte";
     import Alert from "../../UiElements/Alerts/Alert.svelte";
     import RegistrationStep1 from "./RegistrationStep1.svelte";
-</script>
-<script>
-    import {loggedIn, registrationComplete} from "../../AppConfig";
     import RegistrationStep2 from "./RegistrationStep2.svelte";
 </script>
 
