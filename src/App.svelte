5765f64d88ef8037653f66c3d2aae2a48ea2d5b1
Tanzkalmar35 <94364725+Tanzkalmar35@users.noreply.github.com>
2023-06-24 20:17:13 +0200

    Switched from weird to super weird, can access public GitHub account information now.

--- src/App.svelte
+++ src/App.svelte
@@ -5,23 +5,14 @@
     import LoginPage from "./lib/appcomponents/login/LoginPage.svelte";
     import {onMount} from "svelte";
     import {setup} from "./lib/osoperations/Setup";
+    import {fetchGithubRepo} from "./lib/api/github/ApiWrapper";
 
     let currentUrl: string = window.location.pathname;
 
     onMount(async () => {
         if (localStorage.getItem("setupCompleted") === "true") setup()
-
-        async function fetchGithubStats(username) {
-            const response = await fetch(`https://api.github.com/users/${username}`);
-            const data = await response.json();
-            // Display the user's GitHub stats, e.g., total commits.
-            console.log(data);
-        }
-
-        // Call the function with a sample username
-        await fetchGithubStats('Tanzkalmar35');
+        await fetchGithubRepo("Tanzkalmar35");
     });
-
 </script>
 
 <main class="w-screen h-screen flex justify-center text-black bg-accent">
--- src/lib/api/github/ApiWrapper.ts
+++ src/lib/api/github/ApiWrapper.ts
@@ -1,10 +1,20 @@
-import {Octokit} from "@octokit/rest";
+import {Octokit} from '@octokit-next/core';
 
 const octokit = new Octokit();
 
-export async function fetchGithub() {
-    const username: string = "Tanzkalmar35";
-    const response = await octokit.request(`GET /users/${username}`);
-    const data = response.data;
-    console.log("Github data: " + data);
+export async function fetchGithubUsername(username: string): Promise<string> {
+    try {
+        const response = await octokit.request(`GET /users/${username}`);
+        const data: any = response.data;
+        console.log("Github data: " + data.login);
+        //return data;
+        return ""
+    } catch (error) {
+        console.error("Error fetching GitHub data:", error);
+        throw error;
+    }
+}
+
+export async function fetchGithubRepo(username: string): Promise<void> {
+    await fetchGithubUsername(username);
 }
